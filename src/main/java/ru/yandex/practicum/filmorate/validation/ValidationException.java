7eebb3ce0dd78b12de718ef5e6d7aa59ba5253e8
Illarionov Alex <linedaykek@gmail.com>
2023-04-04 22:32:40 +0300

    feat: создание основных типов и контролеров

---
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ValidationException.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.validation;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(final String message) {
+        super(message);
+    }
+}
